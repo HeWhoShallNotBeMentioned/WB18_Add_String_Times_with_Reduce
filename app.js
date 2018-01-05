508f4ac3d9fe41e83c5db005234821a86b7b2efb
Chris Underwood <cunderwoodmn@gmail.com>
2018-01-04 17:04:15 -0800

    split time into mins & secs array and change to number from string

--- app.js
+++ app.js
@@ -2,4 +2,11 @@
 //querySelectorAll returns a node list which can be converted to an array with Array.from or ... spread operator
 const timeNodes = Array.from(document.querySelectorAll('[data-time]'));
 
-const seconds = timeNodes.map(node => node.dataset.time);
+const seconds = timeNodes
+        .map(node => node.dataset.time)
+        .map(timeCode => {
+          const [mins, secs] = timeCode.split(':').map(parseFloat);
+          console.log(mins, secs);
+          return (mins * 60) + secs;
+
+        });

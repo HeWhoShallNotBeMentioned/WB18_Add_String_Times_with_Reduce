dd7608a25ec961b7f509b63265edabc67951ee4a
Chris Underwood <cunderwoodmn@gmail.com>
2018-01-04 16:14:05 -0800

    grab all the data-time nodes and grabbed the dataset time value for each

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
 
+//querySelectorAll returns a node list which can be converted to an array with Array.from or ... spread operator
+const timeNodes = Array.from(document.querySelectorAll('[data-time]'));
+
+const seconds = timeNodes.map(node => node.dataset.time);

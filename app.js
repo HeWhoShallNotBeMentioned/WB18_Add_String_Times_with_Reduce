17054655828d23d008589b2c726abf167bc88c0a
Chris Underwood <cunderwoodmn@gmail.com>
2018-01-04 17:08:18 -0800

    add reduce funciton that will sume all of the seconds together

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const seconds = timeNodes
         .map(node => node.dataset.time)
         .map(timeCode => {
           const [mins, secs] = timeCode.split(':').map(parseFloat);
-          console.log(mins, secs);
           return (mins * 60) + secs;
-
-        });
+        })
+        .reduce((total, vidSeconds) => total + vidSeconds);
